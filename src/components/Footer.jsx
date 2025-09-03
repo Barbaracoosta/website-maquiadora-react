e3edbedac25b7322d2138baaad587fb164ed56da
Barbaracoosta <Babiicoosta@gmail.com>
2025-09-03 14:37:22 -0300

    incluindo mudança de ano automatica

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer>
-      <p>© 2025 Pâmella Borba. Todos os direitos reservados.</p>
+    <footer className="footer">
+      <p>© {currentYear} Pâmella Borba. Todos os direitos reservados.</p>
     </footer>
   );
 }
