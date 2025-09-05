485639027c7883e92fe43f759b492faf4dc8c7e5
Barbaracoosta <babiicoosta2@gmail.com>
2025-09-05 19:52:03 -0300

    Aprimorando visual e responsividade do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaInstagram } from "react-icons/fa";
 import "./Footer.css";
 
 function Footer() {
@@ -5,7 +6,26 @@ function Footer() {
 
   return (
     <footer className="footer">
+      <div className="footer-content">
       <p>© {currentYear} Pâmella Borba. Todos os direitos reservados.</p>
+
+        <div className="footer-links">          
+          <a href="#sobre">Sobre</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#servico">Serviços</a>
+          <a href="#contato">Contato</a>
+        </div>
+        <div className="footer-socials">          
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <FaInstagram />
+          </a>
+
+        </div>
+        <div className="footer-message">
+          Beleza com Proposito! 💜
+        </div>
+
+      </div>
     </footer>
   );
 }
