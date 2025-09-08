4831b1b0a5d066423a05257eea1bd1f06746b3a3
Barbaracoosta <babiicoosta2@gmail.com>
2025-09-08 16:21:23 -0300

    Criando css de galeria de videos, e ajustes nas galerias de fotos e videos

--- src/pages/GaleriaVideos.jsx
+++ src/pages/GaleriaVideos.jsx
@@ -1,6 +1,8 @@
+import './GaleriaVideos.css';
+
 function GaleriaVideos() {
   return (
-    <section id="galeria-videos">
+    <section className="galeriavideo" id="galeria-videos">
       <h2>Galeria de Vídeos</h2>
       <p>Vídeos mostrando bastidores e técnicas.</p>     
     </section>
